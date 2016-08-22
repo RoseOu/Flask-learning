a3524ddd4db60f2445f0b794ff9e4d4993aa7af1
roseou <598959770@qq.com>
2016-08-22 17:04:35 +0800

    2a

--- flaskblog/hello.py
+++ flaskblog/hello.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask.ext.script import Manager  #!
+from flask import Flask, render_template
+from flask.ext.script import Manager
 
 app = Flask(__name__)
-manager = Manager(app)                   #!
+manager = Manager(app)
 
 @app.route('/')
-def index():                              
-    return '<h1>Hello world!</h1>'
+def index():
+    return render_template('index.html')
 
+@app.route('/user/')
 @app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name   
+def user(name=None):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()
