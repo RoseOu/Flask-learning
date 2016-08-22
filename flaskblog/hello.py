92b355a30b78cd3a2f62dc100b143ecc586d1094
roseou <598959770@qq.com>
2016-08-22 17:08:39 +0800

    2b

--- flaskblog/hello.py
+++ flaskblog/hello.py
@@ -13,5 +13,13 @@ def index():
 def user(name=None):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     manager.run()
