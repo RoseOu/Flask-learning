81876c9cc3392b6f6ff380f7c40f7112e5a1b37b
roseou <598959770@qq.com>
2016-08-24 15:45:19 +0800

    5a

--- flaskblog/app/__init__.py
+++ flaskblog/app/__init__.py
@@ -19,7 +19,7 @@ def create_app(config_name):
 
 	bootstrap.init_app(app)
 	db.init_app(app)
-	login_manager.init_app(app)
+	login_manager.init_app(app)         #
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
--- flaskblog/app/main/__init__.py
+++ flaskblog/app/main/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from ..models import Permission
 
 main = Blueprint('main', __name__)
 
 from . import views, errors
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
