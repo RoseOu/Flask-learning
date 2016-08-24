fdf8da677940aae0c4825fedad8c8db660a5113e
roseou <598959770@qq.com>
2016-08-24 09:25:30 +0800

    3c

---
+++ flaskblog/app/main/views.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,session,redirect,url_for
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'))
