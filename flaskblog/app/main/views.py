5f8087d897f91c273f3801bbe66f4d5653b2bfc9
roseou <598959770@qq.com>
2016-08-24 16:25:49 +0800

    6a

--- flaskblog/app/main/views.py
+++ flaskblog/app/main/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,session,redirect,url_for
+from flask import Flask, render_template,session,redirect,url_for,abort
 from . import main
 from .forms import NameForm
 from .. import db
@@ -16,3 +16,12 @@ def index():
         form.name.data = ''
         return redirect(url_for('.index'))
     return render_template('index.html', form=form, name=session.get('name'))
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
+
