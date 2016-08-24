45ca1b70368d13c1ef906af230ee90c6c0d9b476
roseou <598959770@qq.com>
2016-08-24 16:59:41 +0800

    6a

--- flaskblog/app/models.py
+++ flaskblog/app/models.py
@@ -45,7 +45,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)  
-    password_hash = db.Column(db.String(128))                    
+    password_hash = db.Column(db.String(128))       
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())             
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property                 
