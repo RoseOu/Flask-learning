e37ba566751b6c4df581de5a66a4065ebefa6766
roseou <598959770@qq.com>
2016-08-25 10:05:44 +0800

    7b

--- flaskblog/app/main/views.py
+++ flaskblog/app/main/views.py
@@ -22,7 +22,7 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    posts = user.posts.all()
+    posts = user.posts.all()        ##
     return render_template('user.html', user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
